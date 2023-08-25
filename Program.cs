7abdbb917cc9faeca67d71f6abb717fdb4e61c28
BrunoAssengo <brunoassengo@me.com>
2023-08-24 21:08:21 -0300

    salvamento
    metade

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Plantao_UC_COD_BACK.classes;
+string opcao = "";
+PessoaJuridica PJ = PessoaJuridica();
+
+do{
+Console.WriteLine(@"
+
+1 - Cadastrar
+2 - Sair
+
+")?:
+    opcao = Console.ReadLine();
+    if (opcao=="1"){
+        PessoaJuridica = new PessoaJuridica();
+        Console.WriteLine("Digite o nome da Pessoa Juridica");
+        PJ.Nome = Console.ReadLine();
+    }
+}while(opcao != "2")
---
+++ classes/Pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plantao_UC_COD_BACK.classes
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set;}
+        public float Rendimento { get; set;}
+
+    }
+}
